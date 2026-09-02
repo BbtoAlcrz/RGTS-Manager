20258962c36febb029f15dbcd8d48c66abe1c7ae
Benito <benito11kpo@gmail.com>
2026-09-02 20:10:02 -0300

    añadi Entidad rol y usuario

---
+++ RGTS.Entidades/Rol.cs
@@ -0,0 +1,9 @@
+﻿namespace RGTS.Entidades
+{
+    internal class Rol
+    {
+        public int IdRol { get; set; }
+        public required string NombreRol { get; set; }
+        public required string DescripcionRol { get; set; }
+    }
+}
---
+++ RGTS.Entidades/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RGTS.Entidades
+{
+    internal class Usuario
+    {
+        public int IdUsuario { get; set; }
+        public int IdRol { get; set; }
+        public required string Nombre { get; set; }
+        public required string Apellido { get; set; }
+        public required string Email { get; set; }
+        public required string ContrasenaHash { get; set; }
+        public bool Activo { get; set; }
+
+        public string NombreCompleto => $"{Nombre} {Apellido}";
+        public required Rol Rol { get; set; }
+    }
+}
